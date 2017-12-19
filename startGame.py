4f91882bb1880f1cf5efed134306630524768397
Luke Shays <lukeshays@gmail.com>
2017-12-19 15:17:46 -0500

    Implemented folding

--- startGame.py
+++ startGame.py
@@ -3,11 +3,13 @@ from determine import winner, bestHand
 from printer import printCards
 
 def placeBet(chips):
-    bet = raw_input("Enter number of chips to bet or press enter to pass: ")
+    bet = raw_input("<Enter> = pass, <f> = fold\nEnter number of chips to bet: ")
     if not bet:
         return
     else:
         try:
+            if bet.lower() == "f":
+                return bet
             bet = int(bet)
             if bet > chips:
                 print "Oops, you dont have that many chips silly!"
@@ -31,36 +33,48 @@ while True:
     p1Final = temp[0]
     p2Final = temp[1]
 
-    w = winner(p1Final, p2Final)
+    w = list(winner(p1Final, p2Final))
     
     printCards(p2, [0, 0], indented=True)
     printCards(middle, [0, 0, 0, 0, 0], message="Pot: " + str(pot))
     printCards(p1, indented=True)
     print "Chips:", chips
 
+    fold = False
     bet = placeBet(chips)
     if bet:
-        chips -= bet
-        pot += 2*bet
-    print "\n"
-
-    faceUp = [1, 1, 0, 0, 0]
-    while not all(faceUp):
-        for x in range(len(faceUp)):
-            if faceUp[x] == 0:
-                faceUp[x] = 1
-                break
-        printCards(p2, [0, 0], indented=True)
-        printCards(middle, faceUp, message="Pot: " + str(pot))
-        printCards(p1, indented=True)
-        print "Chips:", chips
-        bet = placeBet(chips)
-        if bet:
+        if bet == "f":
+            fold = True
+            w[0] = 2
+            w[2] = ""
+        else:
             chips -= bet
             pot += 2*bet
-        print "\n"
+    print "\n"
 
-    printCards(p2, message=w[2], indented=True)
+    if not fold:
+        faceUp = [1, 1, 0, 0, 0]
+        while not all(faceUp):
+            for x in range(len(faceUp)):
+                if faceUp[x] == 0:
+                    faceUp[x] = 1
+                    break
+            printCards(p2, [0, 0], indented=True)
+            printCards(middle, faceUp, message="Pot: " + str(pot))
+            printCards(p1, indented=True)
+            print "Chips:", chips
+            bet = placeBet(chips)
+            if bet:
+                if bet == "f":
+                    fold = True
+                    w[0] = 2
+                    w[2] = ""
+                    break
+                chips -= bet
+                pot += 2*bet
+            print "\n"
+
+    printCards(p2, [not fold, not fold], message=w[2], indented=True)
     printCards(middle, message="Pot: " + str(pot))
     printCards(p1, message=w[1], indented=True)
     
